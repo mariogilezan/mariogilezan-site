192f76c807722bdcc98d7caef21228b76a3ba158
Mario Gilezan <mariogilezan10@gmail.com>
2021-08-30 18:02:47 +0200

    added gatsby settings in gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,71 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Mario Gilezan`,
+    description: `Portfolio website for Mario Gilezan, front-end developer based in Serbia`,
+    keywords: [
+      "mario gilezan",
+      "web developer",
+      "front-end developer",
+      "react developer",
+      "gatsby",
+      "sanity",
+      "vršac",
+    ],
+    siteUrl: `http://localhost:8000/`,
+    menuLinks: [
+      {
+        name: `Home`,
+        slug: `/`,
+      },
+      {
+        name: `About`,
+        slug: `/about`,
+      },
+      {
+        name: `Projects`,
+        slug: `/projects`,
+      },
+      {
+        name: `Contact`,
+        slug: `/contact`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mario Gilezan`,
+        short_name: `MG`,
+        start_url: `/`,
+        background_color: `#FEFEFE`,
+        theme_color: `#0081CF`,
+        display: `standalone`,
+        icon: `content/assets/page-icon.png`,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        // token: process.env.SANITY_TOKEN,
+        watchMode: process.env.NODE_ENV === "development",
+      },
+    },
+  ],
 }
