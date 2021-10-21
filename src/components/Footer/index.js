00df672b3880a704ceb0a88ffb61cf65b52ee681
Mario Gilezan <mariogilezan10@gmail.com>
2021-10-21 18:15:13 +0200

    added dynamic current year change in footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,13 @@ import { FooterText, FooterWrapper } from "./FooterElements"
 import { TextHighlight } from "../../styles/globalStyles"
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear()
+
   return (
     <>
       <FooterWrapper>
         <FooterText>
-          &copy; 2021 <TextHighlight>M</TextHighlight>ario{" "}
+          &copy; {currentYear} <TextHighlight>M</TextHighlight>ario{" "}
           <TextHighlight>G</TextHighlight>ilezan. All Rights Reserved.
         </FooterText>
       </FooterWrapper>
