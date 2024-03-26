56c2489a4c51f7a8c8c46a1fb95703cdfc7f7174
Mario Gilezan <mariogilezan10@gmail.com>
2024-03-26 21:30:45 +0100

    reorganize component markup and import hamburger menu icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,35 +9,33 @@ import {
   NavLink,
   NavList,
 } from "./NavbarElements"
-import { TextHighlight } from "../../styles/globalStyles"
+import { HamburgerMenu, TextHighlight } from "../../styles/globalStyles"
 import SocialLinks from "../SocialLinks"
 import MobileMenu from "../MobileMenu"
 
 export default function Navbar({ isOpen, toggle }) {
   const { menuLinks } = useSiteMetadata()
   return (
-    <>
-      <Nav>
-        <NavContainer>
-          <Logo to="/">
-            <TextHighlight>M</TextHighlight>G
-          </Logo>
-          <NavList>
-            {menuLinks.map(link => (
-              <NavItem key={link.name}>
-                <NavLink to={link.slug} activeStyle={{ color: "#00a4ea" }}>
-                  {link.name}
-                </NavLink>
-              </NavItem>
-            ))}
-          </NavList>
-          <SocialLinks />
-          <MenuIconWrapper onClick={toggle}>
-            <span>menu</span>
-          </MenuIconWrapper>
-          <MobileMenu isOpen={isOpen} toggle={toggle} />
-        </NavContainer>
-      </Nav>
-    </>
+    <Nav>
+      <NavContainer>
+        <Logo to="/">
+          <TextHighlight>M</TextHighlight>G
+        </Logo>
+        <NavList>
+          {menuLinks.map(link => (
+            <NavItem key={link.name}>
+              <NavLink to={link.slug} activeStyle={{ color: "#00a4ea" }}>
+                {link.name}
+              </NavLink>
+            </NavItem>
+          ))}
+        </NavList>
+        <SocialLinks />
+        <MenuIconWrapper onClick={toggle}>
+          <HamburgerMenu />
+        </MenuIconWrapper>
+        <MobileMenu isOpen={isOpen} toggle={toggle} />
+      </NavContainer>
+    </Nav>
   )
 }
