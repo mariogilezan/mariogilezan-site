1aaa060613fbfbef08faf25ce600e9844a80036d
Mario Gilezan <mariogilezan10@gmail.com>
2024-03-28 23:41:52 +0100

    reorganize index file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,17 @@
 import React from "react"
-import { FooterText, FooterWrapper } from "./FooterElements"
+
 import { TextHighlight } from "../../styles/globalStyles"
+import { FooterText, FooterWrapper } from "./FooterElements"
 
 export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <>
-      <FooterWrapper>
-        <FooterText>
-          &copy; {currentYear} <TextHighlight>M</TextHighlight>ario{" "}
-          <TextHighlight>G</TextHighlight>ilezan. All Rights Reserved.
-        </FooterText>
-      </FooterWrapper>
-    </>
+    <FooterWrapper>
+      <FooterText>
+        &copy; {currentYear} <TextHighlight>M</TextHighlight>ario{" "}
+        <TextHighlight>G</TextHighlight>ilezan. All Rights Reserved.
+      </FooterText>
+    </FooterWrapper>
   )
 }
