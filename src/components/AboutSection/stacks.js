f0eb70fab5398c0bcac883f756d6dde9dce2d1ca
Mario Gilezan <mariogilezan10@gmail.com>
2024-03-28 23:32:34 +0100

    extract all stack logos from index file and export back in index file from stacks file

---
+++ src/components/AboutSection/stacks.js
@@ -0,0 +1,46 @@
+import React from "react"
+import {
+  CssLogo,
+  FramerLogo,
+  GatsbyLogo,
+  HtmlLogo,
+  JavaScriptLogo,
+  ReactLogo,
+  SassLogo,
+  StyledCmpLogo,
+} from "./AboutSectionElements"
+
+export const stacks = [
+  {
+    name: "HTML",
+    logo: <HtmlLogo />,
+  },
+  {
+    name: "CSS",
+    logo: <CssLogo />,
+  },
+  {
+    name: "Sass",
+    logo: <SassLogo />,
+  },
+  {
+    name: "JavaScript",
+    logo: <JavaScriptLogo />,
+  },
+  {
+    name: "React",
+    logo: <ReactLogo />,
+  },
+  {
+    name: "styled-components",
+    logo: <StyledCmpLogo />,
+  },
+  {
+    name: "Framer",
+    logo: <FramerLogo />,
+  },
+  {
+    name: "GatsbyJS",
+    logo: <GatsbyLogo />,
+  },
+]
